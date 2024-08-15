b4111ca9939144bb5210f9d7af128d2eb2c37d32
YunusEmreSimseek <yemresimseek@gmail.com>
2024-08-15 11:57:35 +0300

    init

---
+++ ShowieFlix/ShowieFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowieFlixApp.swift
+//  ShowieFlix
+//
+//  Created by Emre Simsek on 15.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShowieFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
